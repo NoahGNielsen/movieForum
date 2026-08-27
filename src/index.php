f11d063206f57d52d58487014a27cd5b2ea84ddb
NoahGNielsen <github.com.1ersk@passinbox.com>
2026-08-27 14:07:00 +0200

    added some tempory text, disallowed crawlers and added security in .htaccess

--- src/index.php
+++ src/index.php
@@ -7,7 +7,13 @@
 </head>
 <body>
     <main>
-
+        <h1>
+            Velkommen til Pellicula Film Forum!
+        </h1>
+        <br>
+        <p>
+            Dette er et skoleprojekt, der har til formål at demonstrere vores færdigheder inden for webudvikling. Du vil på sigt kunne diskuttere film, dele anmeldelser og deltage i filmrelaterede samtaler med andre filmelskere. Vi håber, du vil nyde din tid her!
+        </p>
     </main>
     <?php include 'assets/php/footer.php'; ?>
 </body>
