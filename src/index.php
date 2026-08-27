026c5f7f27e9ba2820482d223ff553727c75e805
NoahGNielsen <github.com.1ersk@passinbox.com>
2026-08-27 13:50:02 +0200

    tilføjet licens og main css fil

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <title>Hjem - Pellicula Film Forum</title>
+    <meta name="description" content="TEMP">
+    <?php include 'assets/php/header.php'; ?>
+</head>
+<body>
+    <main>
+
+    </main>
+    <?php include 'assets/php/footer.php'; ?>
+</body>
+</html>
