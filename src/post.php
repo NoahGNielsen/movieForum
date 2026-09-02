f20fd92608245a79df2685c350d4af0d71e602f0
NoahGNielsen <github.com.1ersk@passinbox.com>
2026-09-02 17:21:18 +0200

    can't remember what i have done....

---
+++ src/post.php
@@ -0,0 +1,18 @@
+<?php include '../assets/php/userCookieHandeling.php'; ?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <title>Post - Pellicula Film Forum</title>
+    <meta name="description" content="TEMP">
+    <link rel="stylesheet" href="/assets/css/post   .css">
+    <?php include '/assets/php/header.php'; ?>
+</head>
+<body>
+    <main>
+        <h1>
+            Post
+        </h1>
+    </main>
+    <?php include '/assets/php/footer.php'; ?>
+</body>
+</html>
---
+++ src/userProfile.php
@@ -0,0 +1,18 @@
+<?php include '../assets/php/userCookieHandeling.php'; ?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <title>Brugerprofil - Pellicula Film Forum</title>
+    <meta name="description" content="TEMP">
+    <link rel="stylesheet" href="/assets/css/userProfile.css">
+    <?php include '/assets/php/header.php'; ?>
+</head>
+<body>
+    <main>
+        <h1>
+            userProfile
+        </h1>
+    </main>
+    <?php include '/assets/php/footer.php'; ?>
+</body>
+</html>
